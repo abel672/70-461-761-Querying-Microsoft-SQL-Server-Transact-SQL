6a84f6c2884ea10c82fe15d7adb0406dfb7f3770
Abel.Sanchez <abel.sanchez@gvcgroup.com>
2021-01-24 18:54:04 +0100

    Deleting the data, then the table

---
+++ Section 3 Session 1 - Creating-tables/04-Deleting-data-and-table.sql
@@ -0,0 +1,11 @@
+SELECT * FROM tblSecond;
+
+-- Delete from a table
+DELETE FROM tblSecond;
+-- Similar
+TRUNCATE TABLE tblSecond;
+
+SELECT * FROM tblSecond;
+
+-- Delete a table
+DROP TABLE tblFirst;
