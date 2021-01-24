fdced9431db7b5811b80da29b7e2bcc3992c462e
Abel.Sanchez <abel.sanchez@gvcgroup.com>
2021-01-24 21:34:22 +0100

    Integer numbers

--- Section 4 Session 1 - Number types and functions/01-Creating-temporary-variables.sql
+++ Section 4 Session 1 - Number types and functions/01-Creating-temporary-variables.sql
@@ -1,5 +1,10 @@
 -- Declaring a variable
-DECLARE @myvar AS int = 2;
+DECLARE @myvar AS tinyint = 2;
+
+-- Trying to make it negative
+SET @myvar = -3;
+
+SELECT @myvar as minusThreeVariable;
 
 -- Intiate and Instaziate
 DECLARE @myvar2 AS int;
@@ -14,3 +19,19 @@ SET @myvar = 15 + @myvar;
 
 -- SELECT a variable
 SELECT @myvar AS myVariable;
+
+-- smallint
+DECLARE @myvar3 AS smallint = 2000;
+
+SET @myvar3 = @myvar3 * 10;
+
+
+/**
+
+	Bigint
+	Int - up to 2,000,000,000
+	Tinyint - 0-255
+	Smallint - -32767 to 32768
+
+	BITS
+*/
