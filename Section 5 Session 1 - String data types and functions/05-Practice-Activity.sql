dc1b1a42f73fb01d0f0a6f1a3df8b17bde3c5828
Abel.Sanchez <abel.sanchez@gvcgroup.com>
2021-01-26 13:08:17 +0100

    Practice Activity

---
+++ Section 5 Session 1 - String data types and functions/05-Practice-Activity.sql
@@ -0,0 +1,24 @@
+﻿select [name]
+from sys.all_columns;
+
+-- 1
+select 'A' + [name]
+from sys.all_columns
+
+-- adding format character
+select [name] + N'Ⱥ'
+from sys.all_columns
+
+-- removing first character
+select RIGHT([name], LEN([name])-1), [name]
+from sys.all_columns;
+
+select SUBSTRING([name],2, LEN([name])-1), [name]
+from sys.all_columns;
+
+-- removing last character
+select LEFT([name], LEN([name])-1), [name]
+from sys.all_columns;
+
+select SUBSTRING([name], 1, LEN([name])-1), [name]
+from sys.all_columns;
