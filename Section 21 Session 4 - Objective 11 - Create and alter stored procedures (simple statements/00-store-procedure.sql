44b14b68f442103379fa20ea624489fccc08f9b6
Abel.Sanchez <abel.sanchez@gvcgroup.com>
2021-02-11 17:51:36 +0100

    Ask for a specific employee

--- Section 21 Session 4 - Objective 11 - Create and alter stored procedures (simple statements/00-store-procedure.sql
+++ Section 21 Session 4 - Objective 11 - Create and alter stored procedures (simple statements/00-store-procedure.sql
@@ -6,17 +6,35 @@
 
     -EXEC NameEmployees              NameEmployees     Data
                                           SELECT...
-*/
 
-CREATE PROCEDURE NameEmployees as
+Difference between procedure and view:
+
+    In the procedure you can put logic.
+
+    The view is just a select statement.
+        You can get columns from the view
+            SELECT EmployeeNumber from ViewEmployees;
+*/
+                              -- params
+CREATE PROCEDURE NameEmployees(@EmployeeNumber int) as 
 BEGIN
-    SELECT EmployeeNumber, EmployeeFirstName, EmployeeLastName
-    FROM tblEmployee
+    -- logic
+    IF EXISTS (SELECT * FROM tblEmployee WHERE EmployeeNumber = @EmployeeNumber)
+    BEGIN
+        SELECT EmployeeNumber, EmployeeFirstName, EmployeeLastName
+        FROM tblEmployee
+        WHERE EmployeeNumber = @EmployeeNumber
+    END
 END 
 
 GO
 
 -- RUN procedure
-EXECUTE NameEmployees;
-EXEC NameEmployees;
-NameEmployees;
+EXECUTE NameEmployees 323;
+EXEC NameEmployees 223;
+NameEmployees 4;
+
+-- Query our procedures
+-- IF EXISTS (SELECT * FROM sys.procedures WHERE name = 'NameEmployees')
+IF OBJECT_ID('NameEmployees', 'P') IS NOT NULL
+    DROP PROCEDURE NameEmployees;
