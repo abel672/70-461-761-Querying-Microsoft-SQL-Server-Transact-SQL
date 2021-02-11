d34efbfd4c5e60d20d350221b61d3a24f5aa7416
Abel.Sanchez <abel.sanchez@gvcgroup.com>
2021-02-11 11:36:54 +0100

    Merge Statement part 3
    Merge with additional columns

--- Section 20 Session 4 - Objective 13 Combine datasets/04-MERGE.sql
+++ Section 20 Session 4 - Objective 13 Combine datasets/04-MERGE.sql
@@ -1,17 +1,27 @@
 USE [70-461];
 BEGIN TRAN
-    --  MERGE QUERY
-    MERGE INTO tblTransactions as T -- Target table
+    ALTER TABLE tblTransactions
+        ADD Comments varchar(50) NULL;
+    go -- DDL: Data Definition Language
+
+    --  MERGE QUERY: Taking just 5% of the data
+    MERGE TOP(5) PERCENT INTO tblTransactions as T -- Target table -- DML: Data Manipulation Language
     USING (SELECT EmployeeNumber, DateOfTransaction, SUM(Amount) as Amount
         FROM tblTransactionView
         GROUP BY EmployeeNumber, DateOfTransaction) as S  -- Source table
     ON T.EmployeeNumber = S.EmployeeNumber AND T.DateOfTransaction = S.DateOfTransaction
-    WHEN MATCHED THEN -- Match case
-          UPDATE SET Amount = T.Amount + S.Amount
-    WHEN NOT MATCHED BY TARGET THEN -- Not Matched case
-        INSERT (Amount, DateOfTransaction, EmployeeNumber)
-        VALUES (S.Amount, S.DateOfTransaction, S.EmployeeNumber)
-    OUTPUT inserted.*, deleted.*; -- show results;
+    WHEN MATCHED AND T.Amount + S.Amount > 0 THEN -- Match case with new amount
+        UPDATE SET Amount = T.Amount + S.Amount, Comments = 'Updated Row'
+    WHEN MATCHED THEN -- Match with same amount
+        DELETE
+    WHEN NOT MATCHED BY TARGET THEN -- Not Matched case in target
+        INSERT (Amount, DateOfTransaction, EmployeeNumber, Comments)
+        VALUES (S.Amount, S.DateOfTransaction, S.EmployeeNumber, 'Inserted Row')
+    WHEN NOT MATCHED BY SOURCE THEN -- Not Matched case in source
+        UPDATE SET Comments = 'Unchanged'
+    OUTPUT inserted.*, deleted.*, $action; -- show results, + action made in each row;
+
+    SELECT * FROM tblTransactions; -- To see inserted comments in the table
 ROLLBACK TRAN
 
 -- tblTransaction (no) - tblTransactionView (yes)
