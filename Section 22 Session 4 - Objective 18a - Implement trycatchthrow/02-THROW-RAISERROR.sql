8e38f0765f6a61c819a43d5253744edf12ec7482
Abel.Sanchez <abel.sanchez@gvcgroup.com>
2021-02-15 16:48:09 +0100

    THROW and RAISERROR
    To throw custom error messages

---
+++ Section 22 Session 4 - Objective 18a - Implement trycatchthrow/02-THROW-RAISERROR.sql
@@ -0,0 +1,67 @@
+USE [70-461];
+
+IF OBJECT_ID('AverageBalance', 'P') IS NOT NULL
+    DROP PROC AverageBalance
+GO
+
+CREATE PROC AverageBalance(@EmployeeNumberFrom int, @EmployeeNumberTo int, @AverageBalance int OUTPUT) as 
+BEGIN
+    SET NOCOUNT ON
+
+    DECLARE @TotalAmount money
+    DECLARE @NumberOfEmployee int
+
+    -- TRY CATCH
+    BEGIN TRY
+
+        SELECT @TotalAmount = SUM(Amount) FROM tblTransactions
+        WHERE EmployeeNumber BETWEEN @EmployeeNumberFrom AND @EmployeeNumberTo
+
+        SELECT @NumberOfEmployee = COUNT(DISTINCT EmployeeNumber) FROM tblEmployee
+        WHERE EmployeeNumber BETWEEN @EmployeeNumberFrom AND @EmployeeNumberTo
+
+        -- Average
+        SET @AverageBalance = @TotalAmount / @NumberOfEmployee
+
+        RETURN 0
+    END TRY
+
+    BEGIN CATCH
+        SET @AverageBalance = 0
+
+        IF ERROR_NUMBER() = 1834 -- @@ERROR
+            BEGIN
+                RETURN 1834
+            END
+        ELSE
+            RAISERROR ('Another error thrower', 10, 1) -- severity 10 (information)
+            -- THROW 56789, 'Unknonw error throw by my custom procedure', 1
+
+        -- error messages to print
+        -- SELECT ERROR_MESSAGE() as ErrorMessage, ERROR_LINE() as ErrorLine,
+        --         ERROR_NUMBER() as ErrorNumber,  ERROR_PROCEDURE() as ErrorProcedure,
+        --         ERROR_SEVERITY() as ErrorSeverity, -- 0-10 for information
+        --         -- 16 default SQL SERVER log / Windows Application log
+
+        --         -- 20-25
+        --         ERROR_STATE() as ErrorState
+        -- RETURN 1
+    END CATCH
+END
+
+GO
+
+-- This will trhow divided by 0 exception
+DECLARE @AvgBalance int, @ReturnStatus int 
+EXEC @ReturnStatus = AverageBalance 4, 5, @AvgBalance OUTPUT
+SELECT @AvgBalance as MyRowCount, @ReturnStatus as Return_Status
+GO
+
+DECLARE @AvgBalance int, @ReturnStatus int 
+EXEC @ReturnStatus = AverageBalance 223, 227, @AvgBalance OUTPUT
+SELECT @AvgBalance as MyRowCount, @ReturnStatus as Return_Status
+
+GO
+
+DECLARE @AvgBalance int, @ReturnStatus int 
+EXEC @ReturnStatus = AverageBalance @EmployeeNumberFrom = 323, @EmployeeNumberTo = 327, @AverageBalance = @AvgBalance OUTPUT
