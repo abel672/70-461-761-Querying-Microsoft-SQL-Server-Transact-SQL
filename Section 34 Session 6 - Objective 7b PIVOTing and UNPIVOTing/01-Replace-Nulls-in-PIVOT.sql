c29789b2215531b502f4cde87542da6b0fdd6da3
Abel.Sanchez <abel.sanchez@gvcgroup.com>
2021-03-16 16:43:14 +0100

    Replace Null in Pivots

---
+++ Section 34 Session 6 - Objective 7b PIVOTing and UNPIVOTing/01-Replace-Nulls-in-PIVOT.sql
@@ -0,0 +1,19 @@
+WITH myTable as 
+(SELECT YEAR(DateOfTransaction) as TheYear, MONTH(DateOfTransaction) as TheMonth, SUM(Amount) as Amount FROM tblTransactions
+GROUP BY YEAR(DateOfTransaction), MONTH(DateOfTransaction))
+
+-- get total amount per month of each year
+SELECT TheYear, ISNULL([1], 0) as [1], 
+                ISNULL([2], 0) as [2], 
+                ISNULL([3], 0) as [3], 
+                ISNULL([4], 0) as [4], 
+                ISNULL([5], 0) as [5], 
+                ISNULL([6], 0) as [6], 
+                ISNULL([7], 0) as [7], 
+                ISNULL([8], 0) as [8], 
+                ISNULL([9], 0) as [9], 
+                ISNULL([10], 0) as [10], 
+                ISNULL([11], 0) as [11], 
+                ISNULL([12], 0) as [12] FROM myTable
+PIVOT (SUM(Amount) for TheMonth in ([1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12])) as MyPvt
+ORDER BY TheYear
