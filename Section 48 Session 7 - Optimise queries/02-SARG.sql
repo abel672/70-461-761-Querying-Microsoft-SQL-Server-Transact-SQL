2f9448d44aacd414093adc28ad215151f86b08a8
Abel.Sanchez <abel.sanchez@gvcgroup.com>
2021-04-03 11:55:46 +0200

    The cost of SORT to sorting data

--- Section 48 Session 7 - Optimise queries/02-SARG.sql
+++ Section 48 Session 7 - Optimise queries/02-SARG.sql
@@ -11,3 +11,5 @@ FROM tblEmployee as E
 LEFT JOIN tblTransaction as T 
 ON E.EmployeeNumber = T.EmployeeNumber
 WHERE E.EmployeeNumber BETWEEN 340 AND 349 -- SARG: Its seeking our indexes
+-- Don't SORT, unless you need to
+ORDER BY Amount -- SORT Cost a lot of performance (check execution plan of this query and first query)
